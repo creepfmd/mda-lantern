fdbb8384f0fa2d8704194675a48d2c16dbf48f7b
Sergey Mityanin <s.mityanin@gmail.com>
2020-10-28 11:50:41 +0300

    Init

--- The Lantern/ViewController.swift
+++ The Lantern/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentColor: UIColor = .green
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateColor()
     }
 
+    @IBAction func buttonPressed() {
+        updateColor()
+    }
+    
+    fileprivate func updateColor() {
+        var newColor: UIColor
 
+        switch currentColor {
+        case .yellow:
+            newColor = .green
+        case .green:
+            newColor = .red
+        case .red:
+            newColor = .yellow
+        default:
+            newColor = .green
+        }
+
+        view.backgroundColor = newColor
+        currentColor = newColor
+    }
 }
 
